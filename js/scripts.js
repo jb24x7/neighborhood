a9064dc7855750bc23240fb894ef16928d9c342a
James Provance <jb24x7@gmail.com>
2023-01-20 14:30:43 -0800

    add heirarchy to numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,57 @@
 let form = document.getElementById('form');
-let p = document.getElementById('results')
-// let userInput = document.getElementById('number1')
+let p = document.getElementById('results');
 
-function genertator() {
+function generator() {
+
   let number1 = Number(document.querySelector("input#number1").value);
-  let number2 = 1;
   let results = [];
-  p.innerHTML = "";
 
-  // console.log(number1)
-  if (number1 % number2 === 0) {
-    for (let index = number2; index <= number1; index+= number2) {
-      results.push(index)
-      console.log(index)
-    } 
+  for (let index = 0; index <= number1; index++) {
+
+    results.push(index.toString());
+    console.log(index);
 
   }
-  return results
+
+  return results;
+
 }
 
 
 function wordChange(text) {
 
-  const one = ['1'];
-  const two = ['2'];
-  const three = ['3']
-  const usrTxt = text.split(' ');
   const output = [];
 
-  usrTxt.forEach((number) => {
+  text.forEach((number) => {
 
-    if (one.includes(number[0])) {
+    if (number.includes("3")) {
+
+      output.push('"Won\'t you be my neighbor?"')
+
+    } else if (number.includes("2")) {
+
+      output.push('"Boop!"');
+
+    } else if (number.includes("1")) {
 
 
-      output.push("Beep!");
+      output.push('"Beep!"');
 
-    } else if (two.includes(number[0])) {
+    } else {
 
-      output.push("Boop!")
+      output.push(number);
 
-    } else if (three.includes(number[0])) {
-
-      output.push("Won't you be my neighbor?")
     }
 
   });
-  return output
+
+  return output;
 
 }
 
 form.addEventListener("submit", function (event) {
-  let p = document.getElementById('results')
-  // console.log(userInput)
+  let p = document.getElementById('results');
+  p.innerHTML = "";
   event.preventDefault();
-  p.append(wordChange(genertator()).join(' '))
-// p.append(genertator())
+  p.append(wordChange(generator()).join(', '));
 })
