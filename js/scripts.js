1fd802bb844e2f583742b3ab8a0ccd07a535b111
James Provance <jb24x7@gmail.com>
2023-01-20 11:07:06 -0800

    (WIP) getting numbers to change into words

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
+let form = document.getElementById('form');
+let p = document.getElementById('results')
+let userInput = document.getElementById('number1')
 
-let form = document.querySelector("form");
-let p = document.getElementById("results");
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
+function genertator() {
   let number1 = Number(document.querySelector("input#number1").value);
   let number2 = 1;
   let results = [];
@@ -15,6 +14,45 @@ form.addEventListener("submit", function(event) {
       results.push(index)
       console.log(index)
     } 
-    p.append(results.join(", "))
+
   }
-});
+  return results
+}
+
+
+function wordChange(text) {
+
+  const one = ['1'];
+  const two = ['2'];
+  const three = ['3']
+  const usrTxt = text.split(' ');
+  const output = [];
+
+  usrTxt.forEach((number) => {
+
+    if (one.includes(number[0])) {
+
+
+      output.push("Beep!");
+
+    } else if (two.includes(number[0])) {
+
+      output.push("Boop!")
+
+    } else if (three.includes(number[0])) {
+
+      output.push("Won't you be my neighbor?")
+    }
+
+  });
+  return output
+
+}
+
+form.addEventListener("submit", function (event) {
+  let p = document.getElementById('results')
+  // console.log(userInput)
+  event.preventDefault();
+  p.append(wordChange(genertator()).join(' '))
+// p.append(genertator())
+})
